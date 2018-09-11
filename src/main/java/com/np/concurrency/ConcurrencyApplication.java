c335dd13dad0cdc6c3ed7b4f345397d89b2427f8
ningpeng <326726686@qq.com>
2018-09-11 11:13:33 +0800

    init

---
+++ src/main/java/com/np/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,12 @@
+package com.np.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyApplication.class, args);
+	}
+}
